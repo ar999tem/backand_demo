cad274adbf3fac2a93b039201487b353d51633d0
ar999tem <arteem.ershov.gmail.com>
2024-09-28 23:26:08 +0300

    4444

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import express from 'express'
 const app = express()
 const port = process.env.PORT || 4000
 app.get('/', (_req: any, res: any) => {
-    let hellomessage = '33 devdd'
+    let hellomessage = '33 devd44d'
     res.send(hellomessage)
 })
 app.listen(port, () => {
