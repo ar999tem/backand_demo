c8f3463fb102d3a5ed830a5e07c4a1f735c96b83
ar999tem <arteem.ershov.gmail.com>
2024-09-28 22:19:51 +0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express'
+
+const app = express()
+const port = process.env.PORT || 4000
+app.get('/', (_req: any, res: any) => {
+    let hellomessage = '33 devdd'
+    res.send(hellomessage)
+})
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
